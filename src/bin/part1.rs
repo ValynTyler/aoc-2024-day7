432efded02a64295b14f43d4545a96be84538faf
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-07 08:00:23 +0200

    feat: solved part 1

--- src/bin/part1.rs
+++ src/bin/part1.rs
@@ -3,8 +3,14 @@ use aoc_2024_day7::puzzle_input::PuzzleInput;
 fn main() {
     println!("hello from part 1");
 
-    let input_string = include_str!("../../input/example.txt");
+    let input_string = include_str!("../../input/numbers.txt");
     let input = PuzzleInput::from(input_string);
 
-    println!("{:#?}", input);
+    let mut sum = 0;
+    for item in input.0 {
+        let possible = item.is_possible();
+        println!("{:#?}", possible);
+        if possible { sum += item.expect };
+    }
+    println!("{}", sum);
 }
