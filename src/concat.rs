ed1b2628664cc83376acd5df7c3bb1c16d7e570a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-07 09:00:13 +0200

    fix: concat no longer borrowing number

--- src/concat.rs
+++ src/concat.rs
@@ -1,11 +1,11 @@
 use crate::count::Count;
 
 pub trait Concat {
-    fn concat(&self, rhs: Self) -> Self;
+    fn concat(self, rhs: Self) -> Self;
 }
 
 impl Concat for i64 {
-    fn concat(&self, rhs: Self) -> Self {
+    fn concat(self, rhs: Self) -> Self {
         self * i64::pow(10, rhs.digits()) + rhs
     }
 }
