efdae01b49c3f098c00889198fe3474a4c72a518
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-07 09:12:02 +0200

    feat: solutions for part 1 and 2 now separate

--- src/bin/part2.rs
+++ src/bin/part2.rs
@@ -1,3 +1,21 @@
+use aoc_2024_day7::{concat::Concat, puzzle_input::PuzzleInput};
+
 fn main() {
-    println!("hello from part 2")
+    println!("hello from part 2");
+
+    let input_string = include_str!("../../input/example.txt");
+    let input = PuzzleInput::from(input_string);
+
+    let mut sum = 0;
+    for item in input.0 {
+        let possible = item.is_possible(vec![
+            |a, b| a + b,
+            |a, b| a * b,
+            i64::concat
+        ]);
+
+        println!("{:?} {:#?}", item.values, possible);
+        if possible { sum += item.expect };
+    }
+    println!("{}", sum);
 }
